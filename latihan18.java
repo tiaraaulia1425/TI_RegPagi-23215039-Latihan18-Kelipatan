8c05338a866c2c64be4085811db19117f3dcd774
tiaraaulia1425 <tiaraaulia1425@gmail.com>
2024-11-04 15:00:23 +0700

    Add files via upload

---
+++ latihan18.java
@@ -0,0 +1,24 @@
+
+package pertemuan5new;
+
+/**
+ *
+ * Tiara Aulia Maharani
+ * NIM : 23215039
+ * LATIHAN 18 KELIPATAN
+ */
+public class latihan18 {
+    public static void main(String[] args) {
+        double angka = 3.5;
+        
+        System.out.println("Output:");
+        
+      
+        while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
+
+    
